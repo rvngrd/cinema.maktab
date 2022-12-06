a498236b38f8396a307991479a67ed34b31d4b4b
Imanbeigy <imanzb80@gmail.com>
2022-12-06 15:46:17 +0330

    adding new path for showtime_list

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('movie/list/', views.movie_list, name='movie_list'),
     path('movie/details/<int:movie_id>/', views.movie_details, name='movie_details'),
     path('cinema/list/', views.cinema_list, name='cinema_list'),
-    path('cinema/details/<int:cinema_id>/', views.cinema_details, name='cinema_details')
+    path('cinema/details/<int:cinema_id>/', views.cinema_details, name='cinema_details'),
+    path('showtime/list/', views.showtime_list, name='showtime_list')
 ]
