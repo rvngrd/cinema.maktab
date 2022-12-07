37331c58b69b89d2c830674e1d4a59f267677d09
rvngrd <rrvngrdd@gmail.com>
2022-12-07 16:39:14 +0330

    using authenticate() and login() functions

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,21 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
 # Create your views here.
 
 def login_view(request):
-    return render(request, 'accounts/login.html', {})
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            return Http404('نام کاربری یا رمز عبور اشتباه است')
+        else:
+            login(request, user)
+            return HttpResponse('{} خوش آمدید'.format(username))
+    else:
+        return render(request, 'accounts/login.html', {})
 
 def logout_view(request):
     pass
