706ce8892a0a7347016254f5d957c62c117c0dfe
rvngrd <rrvngrdd@gmail.com>
2022-12-08 23:05:23 +0330

    creating Profile model in models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    """
+    Represents user profile
+    """
+
+    class Meta:
+        verbose_name = "نمایه کاربری"
+        verbose_name_plural = "نمایه کاربری"
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='نمایه کاربری')
+    # fields that are stored in User: first_name, last_name, email, date_joined
+
+    mobile = models.CharField('تلفن همراه', max_length=11)
+
+    MALE = 1
+    FEMALE = 2
+    NON_BINARY = 3
+    GENDER_CHOICES = (
+        (MALE, 'مرد'),
+        (FEMALE, 'زن'),
+        (NON_BINARY, 'نان باینری')
+    )
+    gender = models.IntegerField('جنسیت', choices=GENDER_CHOICES, null=True, blank=True)
