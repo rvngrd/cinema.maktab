f32a6db5b4b7ca1724d70a202e87e3c4dbc83ebf
rvngrd <rrvngrdd@gmail.com>
2022-12-09 15:07:32 +0330

    adding profile/details path and creating profile_details in views.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,6 @@ from accounts import views
 app_name = 'accounts'
 urlpatterns = [
     path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout')
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/details', views.profile_details, name='profile_details')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -33,3 +33,6 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse('accounts:login'))
+
+def profile_details(request):
+    pass
