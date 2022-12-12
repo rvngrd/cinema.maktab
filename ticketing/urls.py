f32c076456e310f4924c3968ceeef94080d59041
rvngrd <rrvngrdd@gmail.com>
2022-12-12 18:43:01 +0330

    adding ticket/list and ticket/details urls

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('movie/details/<int:movie_id>/', views.movie_details, name='movie_details'),
     path('cinema/list/', views.cinema_list, name='cinema_list'),
     path('cinema/details/<int:cinema_id>/', views.cinema_details, name='cinema_details'),
-    path('showtime/list/', views.showtime_list, name='showtime_list')
+    path('showtime/list/', views.showtime_list, name='showtime_list'),
+    path('ticket/list/', views.ticket_list, name='ticket_list'),
+    path('ticket/details/<int:ticket_id>/', views.ticket_details, name='ticket_details')
 ]
