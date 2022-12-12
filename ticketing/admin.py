3b2098cc05a628e2fa6333c8d9e16c1afee988c8
rvngrd <rrvngrdd@gmail.com>
2022-12-12 18:50:41 +0330

    registering Ticket model to django admin

--- ticketing/admin.py
+++ ticketing/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from ticketing.models import Movie, Cinema, ShowTime
+from ticketing.models import Movie, Cinema, ShowTime, Ticket
 
 admin.site.register(Movie)
 admin.site.register(Cinema)
 admin.site.register(ShowTime)
+admin.site.register(Ticket)
