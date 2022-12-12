cbdac1776b9fa9624f6ff101b35fad6220857597
rvngrd <rrvngrdd@gmail.com>
2022-12-12 19:16:55 +0330

    removing @login_required decorator for showtime_list

--- ticketing/views.py
+++ ticketing/views.py
@@ -46,7 +46,6 @@ def cinema_details(request, cinema_id):
     return render(request, 'ticketing/cinema_details.html', context)
 
 
-@login_required
 def showtime_list(request):
     showtimes = ShowTime.objects.all().order_by('price')
     context = {
