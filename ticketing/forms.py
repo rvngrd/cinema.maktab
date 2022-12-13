19aad2a95cbfcfe2875fff409b4059a48f586ec1
rvngrd <rrvngrdd@gmail.com>
2022-12-13 21:30:13 +0330

    making required False in movie_name

--- ticketing/forms.py
+++ ticketing/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class ShowTimeSearchForm(forms.Form):
-    movie_name = forms.CharField(max_length=100, label='نام فیلم')
+    movie_name = forms.CharField(max_length=100, label='نام فیلم', required=False)
