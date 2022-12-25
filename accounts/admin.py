2b30471b3ae6be708af1103db5901028c0eb235a
rvngrd <rrvngrdd@gmail.com>
2022-12-25 14:22:43 +0330

    registering Payment model to add some payments in django admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from accounts.models import Profile
+from accounts.models import Profile, Payment
 
 admin.site.register(Profile)
+admin.site.register(Payment)
